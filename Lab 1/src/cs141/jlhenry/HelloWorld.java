bc153402e20aa56c10e6059ffb0e0ee46ef680c3
J3ss62205 <147669349+J3ss62205@users.noreply.github.com>
2023-11-27 12:28:18 -0800

    Hello world lol

---
+++ Lab 1/src/cs141/jlhenry/HelloWorld.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package cs141.jlhenry;
+
+/**
+ * @author jlhenry2
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!"); 
+		// This will print out hello world to the console!!! - JH
+		
+		System.out.println("  * * *  ");
+		System.out.println(" *  *  * ");
+		System.out.println("*   *   *");
+		System.out.println("---------");
+		System.out.println("*   *   *");
+		System.out.println(" *  *  * ");
+		System.out.println("  * * *  ");
+		//This is pattern #1 ^^^ - JH
+		System.out.println(" ");
+		
+		System.out.println(" <-------[ ");
+		System.out.println(" ]-------> ");
+		System.out.println(" <-------] ");
+		System.out.println(" ]-------> ");
+		//This is pattern #2 ^^^ -JH
+		System.out.println("");
+		
+		System.out.println("0   ");
+		System.out.println(" 0  ");
+		System.out.println("  0 ");
+		System.out.println("   0");
+		System.out.println("  0 ");
+		System.out.println(" 0  ");
+		System.out.println("0   ");
+	}
+
+}
